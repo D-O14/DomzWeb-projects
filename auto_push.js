e310d12752e8e5b5863b4b06fb48552b824be066
D-O14 <dominionogwai613@gmail.com>
2026-01-08 14:31:37 +0100

    modified codes

--- auto_push.js
+++ auto_push.js
@@ -1,7 +1,24 @@
-const { exec } = require("child_process");
+/*const { exec } = require("child_process");
 const chokidar = require("chokidar");
 
 const watcher = chokidar.watch(".", { ignored: /node_modules|.git/ });
+
+watcher.on("all", (event, path) => {
+    console.log(`Detected ${event} on ${path}`);
+    exec('git add . && git commit -m "Auto-commit [watcher]" && git push origin main', (err, stdout, stderr) => {
+        if (err) console.error(err);
+        else console.log(stdout);
+    });
+});*/
+
+const { exec } = require("child_process");
+const chokidar = require("chokidar");
+
+const watcher = chokidar.watch(["./src", "./public"], { 
+    ignored: /(\.git|venv|node_modules|\.vscode|\.idea|.*\.pyc|.*~|.*\.log)/,
+    ignoreInitial: true,
+    persistent: true
+});
 
 watcher.on("all", (event, path) => {
     console.log(`Detected ${event} on ${path}`);
