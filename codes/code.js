d8de6c131413e5a1cff200aaa3a93da875b196fd
D-O14 <dominionogwai613@gmail.com>
2026-01-08 22:19:02 +0100

    Modified and added files

--- codes/code.js
+++ codes/code.js
@@ -210,3 +210,35 @@ var cl = cloudinary.Cloudinary.new({ cloud_name: 'dyyvwfgqr', secure: true });
 
 var url = cl.url('sample.jpg', { width: 300, height: 200, crop: 'fill' });
 document.getElementById('myImage').src = url;
+
+
+    const slider = document.querySelector('.slider');
+    document.querySelector('.right').onclick = () => {
+        slider.scrollBy({ left: slider.clientWidth, behavior: "smooth" });
+    };
+
+    document.querySelector('.left').onclick = () => {
+        slider.scrollBy({ left: -slider.clientWidth, behavior: "smooth" });
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const sliderTile = document.querySelector('.tile-slider');
+
+        if (!sliderTile) return;
+
+        let hideTimer;
+
+        function showNav() {
+            sliderTile.classList.add('show-nav');
+
+            clearTimeout(hideTimer);
+            hideTimer = setTimeout(() => {
+                sliderTile.classList.remove('show-nav');
+            }, 2500)
+        }
+
+        ['mousemove', 'click', 'touchstart', 'focusin'].forEach(event => {
+            sliderTile.addEventListener(event, showNav);
+        });
+    });
