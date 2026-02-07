72b1fff407c2236c0763af1ba6cd3d9d38670670
D-O14 <dominionogwai613@gmail.com>
2026-02-07 11:55:16 +0100

    modified

---
+++ src/JS/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase }  from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4mEdLnAaIUkHoRHQDwcAO0XQcEMm79xc",
+  authDomain: "domzweb-projects.firebaseapp.com",
+  projectId: "domzweb-projects",
+  databaseURL: "https://domzweb-projects-default-rtdb.firebaseio.com/",
+  storageBucket: "domzweb-projects.firebasestorage.app",
+  messagingSenderId: "790562424717",
+  appId: "1:790562424717:web:4b552c94385c7bfe2edfc4",
+  measurementId: "G-BQVKFWCVBV"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const analytics = getAnalytics(app);
