5b603e718a52b684b7cb3684b88f9c32ba4c3263
D-O14 <dominionogwai613@gmail.com>
2026-02-18 23:13:44 +0100

    modified

--- references/JS/script2.js
+++
@@ -1,6 +0,0 @@
-const container = document.getElementsByClassName("container");
-const registerBtn = document.getElementById("register");
-
-registerBtn.addEventListener("click", () => {
-	container.classList.add("active");
-});
