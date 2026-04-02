b76d3bb054dec624fddc7fc2eafa044443ae7b67
D-O14 <dominionogwai613@gmail.com>
2026-04-02 21:33:33 +0100

    ulv;vyi

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,6 +1,6 @@
 console.log("Vite is running!");
 
-import "../css/main.css";
+import "src/css/main.css";
 import { db } from "./firebase.config.js";
 import { ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
