78979f4d41e5747fa4c51175f1e4f947900cf223
DomzWeb <dominionogwai613@gmail.com>
2026-07-02 18:42:31 +0100

    updated

--- src/Projects/Intentus/Tasks/tasks-demo.js
+++ src/Projects/Intentus/Tasks/tasks-demo.js
@@ -1,27 +1,30 @@
-
 const tasks = [];
 const tasksSection = document.querySelector(".tasks-content");
 const template = document.querySelector(".task-template");
 const empty = document.querySelector(".empty-template");
-const searchInput = document.querySelector("searchInput");
+const searchInput = document.getElementById("searchInput");
 const taskBtn = document.querySelector(".taskBtn");
-const saveBtn = document.querySelector(".save");
+const saveBtn = document.querySelector(".saveTask");
 const dialog = document.querySelector("dialog");
 const taskInput = document.querySelector(".taskInput");
 
-document.addEventListener("DOMContentLoaded", () => { renderTasks(tasksSection, tasks) });
+document.addEventListener("DOMContentLoaded", () => {
+    renderTasks(tasksSection, tasks);
+    validateTask(taskInput, saveBtn);
+});
 
-function addTask() {
+function addTask(array, input) {
     const task = {
         id: crypto.randomUUID(),
-        taskName: taskInput.value.trim(),
+        taskName: input.value.trim(),
         createdAt: getCurrentTime(),
         alarm: null,
         completed: false,
+        tags: [null, null],
     };
 
-    tasks.unshift(task);
-    taskInput.value = "";
+    array.unshift(task);
+    input.value = "";
 
     renderTasks(tasksSection, tasks);
 }
@@ -47,19 +50,44 @@ function renderTasks(section, array) {
         task.querySelector(".task-title").textContent = arr.taskName;
         task.querySelector(".task-date").textContent = arr.createdAt;
         task.querySelector(".check").id = arr.id;
-        task.querySelector(".label").htmlFor = arr.id;
+        task.querySelector(".content").htmlFor = arr.id;
         section.append(task);
     });
 };
 
-function validateTask() {
-    if (taskInput.value.trim() === "") {
-        saveBtn.setAttribute("disabled", "true");
+function validateTask(input, button) {
+    if (input.value.trim() === "") {
+        button.setAttribute("disabled", "true");
     } else {
-        saveBtn.removeAttribute("disabled");
+        button.removeAttribute("disabled");
     };
 }
 
-taskBtn.addEventListener("click", () => { dialog.showModal();  /*addTask();*/ });
-saveBtn.addEventListener("click", () => { dialog.close() });
-taskInput.addEventListener("input", () => { validateTask(); });
+function searchTasks(array, input) {
+    let searched = input.value.trim().toLowerCase();
+    const searchedTasks = array.filter(arr => {
+        return arr.taskName.toLowerCase().includes(searched);
+    });
+    renderTasks(tasksSection, searchedTasks);
+}
+
+searchInput.addEventListener("input", () => { searchTasks(tasks, searchInput) });
+
+taskBtn.addEventListener("click", () => {
+    dialog.showModal();
+    dialog.classList.add("open");
+});
+
+saveBtn.addEventListener("click", () => {
+    dialog.classList.remove("open");
+    dialog.classList.add("closing");
+    addTask(tasks, taskInput);
+    setTimeout(() => {
+        dialog.close();
+        dialog.classList.remove("closing");
+    }, 500);
+});
+
+taskInput.addEventListener("input", () => {
+    validateTask(taskInput, saveBtn);
+});
