c72b10d95bcc47c003ebf2ea176942740dd1254b
DomzWeb <dominionogwai613@gmail.com>
2026-07-14 23:58:52 +0100

    updated

--- src/Components/sidebar/sidebar.js
+++ src/Components/sidebar/sidebar.js
@@ -2,6 +2,11 @@ const icons = {
     todo: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide 
     lucide-square-check-big-icon lucide-square-check-big"><path d="M21 10.656V19a2 2 0 0 1-2 2H5a2 2 0 0 
-    1-2-2V5a2 2 0 0 1 2-2h12.344"/>
-    <path d="m9 11 3 3L22 4"/></svg>`,
+    1-2-2V5a2 2 0 0 1 2-2h12.344"/><path d="m9 11 3 3L22 4"/>
+    </svg>`,
+    sidebar: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="lucide lucide-panel-right-icon lucide-panel-right">
+    <rect width="18" height="18" x="3" y="3" rx="2"/><path d="M15 3v18"/>
+    </svg>`
 }
