8b0e417a8e70222ca8daddd20ca9055d2a73d648
DomzWeb <dominionogwai613@gmail.com>
2026-07-17 11:09:44 +0100

    updated

--- public/assets/svgs/svg-library.md
+++ public/assets/svgs/svg-library.md
@@ -272,3 +272,26 @@ class="lucide lucide-archive-icon lucide-archive">
 <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
 </svg>
 ```
+
+# pie chart
+```xml
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+class="lucide pie-chart">
+<path d="M21 12c.552 0 1.005-.449.95-.998a10 10 0 0 0-8.953-8.951c-.55-.055-.998.398-.998.95v8a1 1 0 0 0 1 1z" />
+<path d="M21.21 15.89A10 10 0 1 1 8 2.83" />
+</svg>
+```
+# line chart
+```xml
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+class="lucide line-chart">
+<path d="M12 16v5" />
+<path d="M16 14.639V21" />
+<path d="M20 10.656V21" />
+<path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15" />
+<path d="M4 18.463V21" />
+<path d="M8 14.656V21" />
+</svg>
+```
---
+++ src/Components/sidebar/mySidebar/mySidebar.js
@@ -0,0 +1,53 @@
+
+const indicator = document.querySelector(".active-indicator");
+const active = document.querySelector(".active");
+//const themeBtn = document.querySelector(".themeBtn");
+const toggleBtn = document.querySelector(".toggle-btn");
+const sidebar = document.querySelector(".sidebar");
+const menuBtn = document.querySelector(".menu-btn");
+const subMenu = document.querySelector(".sub-menu");
+const links = document.querySelectorAll(".link");
+const anchors = document.querySelectorAll("a");
+
+indicator.style.top = active.offsetTop + "px";
+indicator.style.height = active.offsetHeight + "px";
+
+/*themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("darkmode");
+});*/
+
+anchors.forEach(a => {
+    a.addEventListener("click", (e) => {
+        e.preventDefault();
+    })
+})
+
+menuBtn.addEventListener("click", () => {
+    if (subMenu.classList.contains("open")) {
+        subMenu.classList.remove("open");
+        subMenu.style.height = "0px";
+    } else {
+        subMenu.classList.add("open");
+        subMenu.style.height = subMenu.scrollHeight + "px";
+    }
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(link => { link.classList.remove("active") });
+        indicator.style.top = link.offsetTop + "px";
+        indicator.style.height = link.offsetHeight + "px";
+        indicator.classList.add("moving");
+        /*indicator.addEventListener("transitionend", () => {
+            links.forEach(link => { link.classList.remove("active") });
+            link.classList.add("active");
+        });*/
+        setTimeout(() => {
+            indicator.classList.remove("moving");
+        }, 300);
+    })
+});
+
+toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed");
+});
