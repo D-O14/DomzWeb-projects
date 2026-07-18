800997188231196fbe74b41f6be66a1c24b61916
DomzWeb <dominionogwai613@gmail.com>
2026-07-18 10:43:45 +0100

    updated

--- src/Components/sidebar/sidebar-dashboard/sidebar.js
+++ src/Components/sidebar/sidebar-dashboard/sidebar.js
@@ -1,6 +1,7 @@
 const indicator = document.querySelector(".active-indicator");
 const toggleBtn = document.querySelector(".toggle-btn");
 const sidebar = document.querySelector(".sidebar");
+
 const template = document.querySelector("template");
 const dashboardMenu = document.querySelector(".menu-dashboard");
 const dropdownMenu = document.querySelector(".menu-dropdown");
@@ -94,7 +95,6 @@ icons = {
                 d="M17.774 10.31a1.12 1.12 0 0 0-1.549 0 2.5 2.5 0 0 1-3.451 0 1.12 1.12 0 0 0-1.548 0 2.5 2.5 0 0 1-3.452 0 1.12 1.12 0 0 0-1.549 0 2.5 2.5 0 0 1-3.77-3.248l2.889-4.184A2 2 0 0 1 7 2h10a2 2 0 0 1 1.653.873l2.895 4.192a2.5 2.5 0 0 1-3.774 3.244" />
             <path d="M4 10.95V19a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8.05" />
         </svg>`,
-
 }
 
 
