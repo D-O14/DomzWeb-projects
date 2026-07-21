1ba95e46edb26caaeeb4f7630ce58c40b6c36896
DomzWeb <dominionogwai613@gmail.com>
2026-07-21 21:09:31 +0100

    updated

--- src/Components/forms/progress/progress-form.js
+++ src/Components/forms/progress/progress-form.js
@@ -3,57 +3,54 @@ const nextBtn = document.querySelectorAll(".nextBtn");
 const backBtn = document.querySelectorAll(".backBtn");
 const mainForm = document.querySelectorAll(".main");
 const steps = document.querySelectorAll("li");
-const stepNum = document.querySelectorAll(".step-num");
+const stepContent = document.querySelectorAll(".step-content");
+const stepNum = document.querySelector(".step-num");
 const dropArea = document.querySelector(".drop-box");
 const button = dropArea.querySelector("button");
 const input = dropArea.querySelector("input");
-const stepNumContent = document.querySelectorAll(".steps-content");
 let index = 0;
 
-nextBtn.forEach(function(nextBtnForm){
-    nextBtnForm.addEventListener("click", () => {
+nextBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
         index++;
         updateForm();
         progressForward();
         contentChange();
-    })
+    });
 });
 
-backBtn.forEach(function (backBtnForm) {
-    backBtnForm.addEventListener("click", () => {
+backBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
         index--;
         updateForm();
         progressBackward()
         contentChange();
-    })
+    });
 });
 
 function updateForm() {
-    mainForm.forEach(main => {
-        main.classList.remove("active");
-    })
+    mainForm.forEach(main => { main.classList.remove("active") });
     mainForm[index].classList.add("active");
 }
 
 function progressForward() {
-    stepNum.innerHTML = index+1;
+    stepNum.innerHTML = index + 1;
     steps[index].classList.add("active");
 }
 
 function progressBackward() {
     let formNum = index + 1;
-    steps[formNum].classList.remove("active");
     stepNum.innerHTML = formNum;
+    steps[formNum].classList.remove("active");
 }
 
 function contentChange() {
-    stepNumContent.forEach(function (content) {
-        content.classList.remove("active");
-        content.classList.add("d-none");
+    stepContent.forEach(step => {
+        step.classList.remove("active");
+        step.classList.add("d-none");
     });
-    stepNumContent[index].classList.add("active")
+    stepContent[index].classList.remove("d-none");
+    stepContent[index].classList.add("active");
 }
 
-button.onclick = () => {
-    input.click();
-}
+button.onclick = () => { input.click() };
