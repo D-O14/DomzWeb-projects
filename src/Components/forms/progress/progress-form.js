ca935eafb7569162bffbfc564ed228673ea7960d
DomzWeb <dominionogwai613@gmail.com>
2026-07-23 13:02:20 +0100

    updated

--- src/Components/forms/progress/progress-form.js
+++ src/Components/forms/progress/progress-form.js
@@ -2,7 +2,6 @@ import { icons } from "../../../Assets/Icons/icons.js";
 
 const steps = document.querySelectorAll("li");
 const formsContainer = document.querySelector(".forms");
-//const stepNum = document.querySelector(".step-num");
 const stepContent = document.querySelectorAll(".step-content");
 const formsTemplate = document.querySelectorAll(".form-template");
 const formCompleted = document.querySelector(".form-completed");
@@ -24,7 +23,25 @@ const nxtBtn = formsContainer.querySelectorAll(".nxtBtn");
 const backBtn = formsContainer.querySelectorAll(".backBtn");
 const dropBox = formsContainer.querySelector(".drop-box");
 const input = dropBox.querySelector("input");
+
+const inputs = document.querySelectorAll("[required]");
 const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const numberInput = document.getElementById("number");
+const addressInput = document.getElementById("address");
+const textArea = document.querySelector("textarea");
+
+textArea.addEventListener("input", () => { charCount() });
+
+inputs.forEach(input => {
+    input.addEventListener("input", () => {
+        validateInput(inputs, input);
+    });
+    if (input === nameInput) { validateName  };
+    if (input === emailInput) { validateEmail };
+    if (input === numberInput) { validateNumber };
+    if (input === addressInput) { validateAddress };
+});
 
 nxtBtn.forEach(btn => {
     btn.innerHTML += icons.chevronRight;
@@ -66,25 +83,101 @@ function updateForm() {
 }
 
 function progressForward() { steps[index].classList.add("active") };
-
 function progressBackward() {
     let formNum = index + 1;
     steps[formNum].classList.remove("active");
 }
 
+function validateName() {
+    if (nameInput.value.trim() === "") {
+        nameInput.setCustomValidity("Name field must not be empty!");
+        showError(nameInput, "Name field must not be empty!");
+        return false;
+    } else {
+        clearError(nameInput);
+        nameInput.setCustomValidity("");
+        return true;
+    };
+}
+
+function validateEmail() {
+    if (emailInput.value.trim() === "") {
+        emailInput.setCustomValidity("E-mail must be provided!");
+        showError(emailInput, "E-mail must be provided!");
+        return false;
+    } else {
+        clearError(emailInput);
+        emailInput.setCustomValidity("");
+        return true;
+    };
+}
+
+function validateNumber() {
+    if (numberInput.value.trim() === "") {
+        numberInput.setCustomValidity("A phone number is required!");
+        showError(numberInput, "A phone number is required!");
+        return false;
+    } else {
+        clearError(numberInput);
+        numberInput.setCustomValidity("");
+        return true;
+    };
+}
+
+function validateAddress() {
+    if (addressInput.value.trim() === "") {
+        addressInput.setCustomValidity("Your house address is needed!");
+        showError(addressInput, "Your house address is needed!");
+        return false;
+    } else {
+        addressInput.setCustomValidity("");
+        clearError(addressInput);
+        return true;
+    };
+}
+
+function validateInput(inputs, field) {
+    inputs.forEach(input => {
+        const name = input.name;
+        if (input.value.trim() === "") {
+            showError(input, "This field must not be empty");
+            input.setCustomValidity(`${ name } is required!`);
+            return false;
+        } else {
+            clearError(input);
+            input.setCustomValidity("");
+            return true;
+        }
+    })
+};
+
+function charCount() {
+    const label = textArea.closest("label");
+    const charCount = label.querySelector("p");
+    const max = textArea.getAttribute("maxlength");
+    const chars = textArea.value.length;
+    charCount.textContent = `${ chars } / ${ max } characters`;
+};
+
 function showError(input, message) {
-    //const field = input.closest("label");
-    const error = input.closest(".textError");
-    input.classList.add("error");
+    const inputDiv = input.closest("div");
+    const icon = inputDiv.querySelector("span");
+    const field = input.closest("label");
+    const error = field.querySelector(".error-text");
+    inputDiv.classList.add("error");
+    icon.classList.add("error");
     error.textContent = message;
-    console.log(input);
-    console.log(error);
     return false;
 }
 
-function clearError(input) { 
-    //const error = input.closest(".textError");
-    input.classList.remove("error");
+function clearError(input) {
+    const inputDiv = input.closest("div");
+    const field = input.closest("label");
+    const icon = inputDiv.querySelector("span");
+    const error = field.querySelector(".error-text");
+    inputDiv.classList.remove("error");
+    icon.classList.remove("error");
+    error.textContent = "";
     return true;
 }
 
