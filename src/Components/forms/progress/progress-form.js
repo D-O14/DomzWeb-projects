1df4d2b96e4c80c15a29a097091806de7d6c07ab
DomzWeb <dominionogwai613@gmail.com>
2026-07-24 14:40:56 +0100

    updated

--- src/Components/forms/progress/progress-form.js
+++ src/Components/forms/progress/progress-form.js
@@ -17,6 +17,12 @@ function renderForms(template, container) {
 
 renderForms(formsTemplate, formsContainer);
 
+const validators = [
+    validateInfoForm,
+    validateEducationForm,
+    validateWorkForm,
+];
+
 const forms = formsContainer.querySelectorAll("form");
 const submitBtn = formsContainer.querySelector(".submitBtn");
 const nxtBtn = formsContainer.querySelectorAll(".nxtBtn");
@@ -27,8 +33,15 @@ const input = dropBox.querySelector("input");
 const inputs = document.querySelectorAll("[required]");
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
-const numberInput = document.getElementById("number");
+const phoneInput = document.getElementById("number");
 const addressInput = document.getElementById("address");
+const dateInput = document.getElementById("date");
+const schoolInput = document.getElementById("school");
+const studyInput = document.getElementById("study");
+const degreeInput = document.getElementById("degree");
+const companyInput = document.getElementById("company");
+const roleInput = document.getElementById("role");
+const yearsInput = document.getElementById("years");
 const textArea = document.querySelector("textarea");
 
 textArea.addEventListener("input", () => { charCount() });
@@ -38,20 +51,22 @@ inputs.forEach(input => {
         validateInput(input);
         if (input === nameInput) { validateName() };
         if (input === emailInput) { validateEmail() };
-        if (input === numberInput) { validateNumber() };
+        if (input === phoneInput) { validatePhone() };
         if (input === addressInput) { validateAddress() };
+        if (input === schoolInput) { validateSchool() };
+        if (input === dateInput) { validateDate() };
+        if (input === studyInput) { validateStudy() };
+        if (input === degreeInput) { validateDegree() };
+        if (input === companyInput) { validateCompany() };
+        if (input === roleInput) { validateRole() };
+        if (input === yearsInput) { validateYears() };
     });
 });
 
 nxtBtn.forEach(btn => {
     btn.innerHTML += icons.chevronRight;
     btn.addEventListener("click", () => {
-        const valid =
-            validateName() &&
-            validateEmail() &&
-            validateName() &&
-            validateAddress();
-        if (!valid) return;
+        if (!validators[index]()) return;
         index++;
         updateForm();
         progressForward();
@@ -89,11 +104,37 @@ function progressBackward() {
     steps[formNum].classList.remove("active");
 }
 
+function validateInfoForm() {
+    return (
+        validateName() &&
+        validateEmail() &&
+        validatePhone() &&
+        validateAddress()
+    );
+}
+
+function validateEducationForm() {
+    return (
+        validateSchool() &&
+        validateDate() &&
+        validateStudy() &&
+        validateDegree()
+    );
+}
+
+function validateWorkForm() {
+    return (
+        validateCompany() &&
+        validateRole() &&
+        validateYears()
+    );
+}
+
 function validateName() {
     if (!validateInput(nameInput)) return false;
     nameInput.value = nameInput.value.replace(/\d/g, "");
     nameInput.value = nameInput.value.replace(/\b\w/g, char => char.toUpperCase());
-    if (nameInput.value === "User") {
+    if (nameInput.value === "User" || nameInput.value === "Admin") {
         showError(nameInput, "Oi! You can't do that!");
         return false;
     } else {
@@ -108,15 +149,65 @@ function validateEmail() {
     return true;
 };
 
-function validateNumber() {
-    if (!validateInput(numberInput)) return false;
-    numberInput.value = numberInput.value.replace(/[A-Za-z]/, "");
+function validatePhone() {
+    if (!validateInput(phoneInput)) return false;
+    phoneInput.value = phoneInput.value.replace(/[A-Za-z]/, "");
     return true;
 };
 
 function validateAddress() {
     if (!validateInput(addressInput)) return false;
     return true;
+};
+
+function validateSchool() {
+    if (!validateInput(schoolInput)) return false;
+    schoolInput.value = schoolInput.value.replace(/\b\w/g, char => char.toUpperCase());
+    return true;
+}
+
+function validateDate() {
+    if (!validateInput(dateInput)) return false;
+    const selectedDate = new Date(dateInput.value);
+    dateInput.max = new Date().toISOString().split("T")[0];
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    if (selectedDate > today) {
+        showError(dateInput, "You can't sign this form if you haven't graudated!");
+        return false;
+    } else {
+        clearError(dateInput);
+        return true;
+    }
+}
+
+function validateStudy() { 
+    if (!validateInput(studyInput)) return false;
+    studyInput.value = studyInput.value.replace(/\b\w/g, char => char.toUpperCase());
+    return true;
+};
+
+function validateDegree() { 
+    if (!validateInput(degreeInput)) return false;
+    degreeInput.value = degreeInput.value.replace(/\b\w/g, char => char.toUpperCase());
+    return true;
+};
+
+function validateCompany() { 
+    if (!validateInput(companyInput)) return false;
+    companyInput.value = companyInput.value.replace(/\b\w/g, char => char.toUpperCase());
+    return true;
+};
+
+function validateRole() { 
+    if (!validateInput(roleInput)) return false;
+    roleInput.value = roleInput.value.replace(/\b\w/g, char => char.toUpperCase());
+    return true;
+};
+
+function validateYears() { 
+    if (!validateInput(yearsInput)) return false;
+    return true
 };
 
 function validateInput(input) {
