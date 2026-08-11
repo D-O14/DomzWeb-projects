02deeeb7a0f048ba33fd3854dd0a9d9edb0632b6
D-O14 <dominionogwai613@gmail.com>
2026-08-11 22:56:49 +0100

    updated

---
+++ src/Components/dialogs/docs/readme.md
@@ -0,0 +1,6 @@
+# Dialog
+# Purpose 
+A UI-blocking component that conveys information, or confirms a user decision, or event about to happen. 
+
+# Completed
+- []
---
+++ src/Components/toast/progress/toast.js
@@ -0,0 +1,25 @@
+import "./toast.css";
+import "@styles/tailwindcss/output.css";
+import { icons, initializeIcons } from "@assets/Icons/icons.js";
+
+const button = document.querySelector("button");
+const toast = document.querySelector(".toast");
+const close = document.querySelector(".close");
+const progress = document.querySelector(".progress");
+
+button.addEventListener("click", () => {
+    toast.classList.add("visible");
+    setTimeout(() => {
+        toast.classList.add("hide");
+    }, 5000);
+    setTimeout(() => {
+        toast.classList.remove("visible");
+    }, 5300);
+
+});
+
+close.addEventListener("click", () => {
+    toast.classList.add("hide");
+});
+
+initializeIcons(document);
