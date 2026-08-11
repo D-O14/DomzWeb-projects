8d442c364c7a2816c1f43f36861a3f306764b5db
D-O14 <dominionogwai613@gmail.com>
2026-08-11 11:14:05 +0100

    updated

---
+++ src/Components/dialogs/confirm dialog/options/confirm-dialog.js
@@ -0,0 +1,5 @@
+import "@styles/tailwindcss/output.css";
+import "./confirm-dialog.css";
+import { icons, initializeIcons } from "@assets/Icons/icons.js";
+
+document.addEventListener("DOMContentLoaded", () => { initializeIcons(document) });
