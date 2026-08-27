e959931b823bb3d021198c7ea5f5fa901f2cd8ec
D-O14 <dominionogwai613@gmail.com>
2026-08-27 12:42:22 +0100

    milestone

--- src/Utilities/actions.js
+++ src/Utilities/actions.js
@@ -1,4 +1,5 @@
 //import "../Components/toast/toast.js";
+import { initializeIcons } from "../Assets/Icons/icons";
 
 export function copy(text, toast) {
     navigator.clipboard.writeText(text);
@@ -33,4 +34,67 @@ export async function share(data) {
             console.error("Couldn't copy text:", error);
         }
     }
+}
+
+export function showLoader(btn) {
+    const icon = btn.querySelector(".icon");
+    btn.classList.add("loading");
+    icon.dataset.icon = "bounce";
+    initializeIcons(btn);
+}
+
+export function hideLoader(btn) {
+    const icon = btn.querySelector(".icon");
+    btn.classList.remove("loading");
+    icon.dataset.icon = "";
+    icon.textContent = "";
+}
+
+export function closeDialog(dialog) {
+    dialog.classList.remove("open");
+    dialog.classList.add("closing");
+    setTimeout(() => {
+        dialog.close();
+        dialog.classList.remove("closing");
+    }, 400);
+}
+
+export function drawPaths(paths) {
+    paths.forEach(path => {
+        if (path) {
+            path.classList.add("draw");
+            const pathLength = path.getTotalLength();
+            const dashArray = path.style.strokeDasharray = pathLength;
+            const dashOffset = path.style.strokeDashoffset = pathLength;
+        };
+    });
+};
+
+export function animateIcon(btn) { 
+    const icon = btn.querySelector(".icon");
+    icon.classList.add("animate");
+    void icon.offsetWidth;
+    icon.classList.add("animate");
+};
+
+export function animateIcons(btn) { 
+    const animation = btn.dataset.iconAnimation;
+    const icon = btn.querySelector("svg");
+    icon.classList.add(`icon-${ animation }`);
+    icon.addEventListener("animationend", () => {
+        icon.classList.remove(`icon-${ animation }`)
+    }, { once: true });
+};
+
+
+export function createRipple(e, btn) {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    const ripple = document.createElement("span");
+    ripple.style.left = `${ x }px`;
+    ripple.style.top = `${ y }px`;
+    btn.append(ripple);
+    btn.classList.add("active");
+    setTimeout(() => { btn.classList.remove("active") }, 300);
+    setTimeout(() => { ripple.remove() }, 600);
 }
--- src/Utilities/date.js
+++ src/Utilities/date.js
@@ -24,3 +24,21 @@ export function validateDate(input, rules) {
         return true;
     }
 }
+
+export function getCurrentTime() {
+    const hours = String(new Date().getHours()).padStart(2, "0");
+    const minutes = String(new Date().getMinutes()).padStart(2, "0");
+    const suffix = hours >= 12 ? "PM" : "AM";
+    return `${ hours }:${ minutes }${ suffix }`;
+}
+
+export function formatTime() {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    //hours = hours % 12 || 12;
+    return `${ padZero(hours) }:${ padZero(minutes) }:${ padZero(seconds) } ${ meridiem }`;
+};
+
+export function padZero(num) { return (num < 10 ? "0" : "") + num };
